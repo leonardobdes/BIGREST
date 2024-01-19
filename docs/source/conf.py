d5dddb1c56b6c7f7bc29e8071b4d9036d6d843bf
Leonardo Souza <42446537+leonardobdes@users.noreply.github.com>
2024-01-19 19:00:26 +0000

    Fixed read the docs file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'BIGREST'
-copyright = '2020, Leonardo Souza'
+copyright = 'Leonardo Souza'
 author = 'Leonardo Souza'
 
 # The full version, including alpha/beta/rc tags
